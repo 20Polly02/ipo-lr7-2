c4adb8676aa4ed67f867fb123709a924f679f621
Polly Bel <polialekseevna80@gmail.com>
2024-12-11 10:23:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-import json#подключаем модуль json
-num_id = int(input("Введите номер квалификации: "))# ввод с клавиатуры номера квалификации 
-find = False #создаем переменную которая будет указывать на то, найдена ли такая квалификация
+import json  # импортируем библиоеку json
 
-with open("dump.json", 'r', encoding='utf-8') as file: #открываем файл для чтения
-  file_r = json.load(file) #загружаем все содержимое файла в переменную file_r
-  for skill in file_r:# используем цикл for чтобы пройтись по всем элементам из файла 
-    if skill.get("model") == "data.skill": #проверяется является ли значение "model" = data.skill"
-      if skill["fields"].get("specialty") == num_id: #проверяется, совпадает ли введенный код квалификации с кодом из файла
-        skill_code = skill["fields"].get("code")#если совпадает, сохраняем код и название квалификации в переменные 
-        skill_title = skill["fields"].get("title")
-        find = True#т.к. калификация найдена придаем переменной find значение true
+num_code = input("Введите номер квалификации: ")  #ввод ноера квалификации от пользователя
+find = False  # инициализируем переменную find как False
 
-        for speciality in file_r:#с помощью цикла for перебираем каждый элемент в файле
-          if speciality.get("model") == "data.specialty":# с помощью цикла проверяем, является ли значение "model" == "data.specialty" 
-            code_speciality = speciality["fields"].get("code")# если совпадает, сохраняем код специальности
-            if code_speciality in skill_code: # с помощью цикла проверяем, содержится ли код специальности в коде квалификации
-              title_speciality = speciality["fields"].get("title")#если содержится, сохраняем название специальности
-              type_speciality = speciality["fields"].get("c_type")#если содержится, сохраняем тип образования
-if not find:#с помощью цикла проверяем, найдена ли квалификация
-  print("=============== Не Найдено ===============") #если не навдена ,вывод результата на консоль
-else:#если квалификация найдена
-  print("=============== Найдено ===============") #вывод на консоль результата
-  print(f"{code_speciality} >> Специальность '{title_speciality}' , {type_speciality}")#вывод на консоль
-  print(f"{skill_code} >> Квалификация '{skill_title}'")#вывод на консоль
+with open("dump.json", 'r', encoding='utf-8') as file:  # открываем файл dump.json для чтения
+    info_file = json.load(file)  # загружаем содержимое файла в переменную info_file
+    for skill in info_file:  # перебираем каждый элемент в info_file
+        if skill.get("model") == "data.skill":  # проверяем, является ли значение "model" = data.skill"
+            if skill["fields"].get("code") == num_code:  # проверяем, совпадает ли код квалификации с введенным
+                skill_code = skill["fields"].get("code")  # получаем код квалификации
+                skill_title = skill["fields"].get("title")  # получаем название квалификации
+                skill_specialty=skill["fields"].get("specialty") #получаем код специальности
+                find = True  # так как квалификация найдена
+                
+if not find:  # если переменная не найдена
+    print("Не найдено")  #вывод на консоль
+    exit()
+for specialty in info_file:  # перебираем каждый элемент в info_file
+    if specialty.get("model") == "data.specialty":  # проверяем, является ли значение "model" = data.specialty"
+        specialty_code = specialty["fields"].get("code")  # получаем код специальности
+        specialty_pk = specialty.get("pk") #получаем код pk
+
+        if skill_specialty == specialty_pk:  # проверяем, одинаковое ли значение у pk и кода специальности 
+            specialty_title = specialty["fields"].get("title")  # получаем название специальности
+            specialty_educational = specialty["fields"].get("c_type")  # получаем тип образования 
+            specialty_c = specialty["fields"].get("code")  # получаем код специальности
+
+else:  # если квалификация найдена
+    print("Найдено") # вывод на консоль
+    print(f"{specialty_c} >> Специальность '{specialty_title}', {specialty_educational}")  #вывод на консоль 
+    print(f"{num_kv} >> Квалификация '{skill_title}'")  #вывод на консоль 
